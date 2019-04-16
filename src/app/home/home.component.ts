b6eded24173590fb19525b2f4b3a991ab77b2d23
RCOliveira98 <respondecodigos@gmail.com>
2019-04-16 13:03:13 -0300

    definition structered home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public dirImg = 'assets/imagens/classroom.jpg';
   constructor() { }
 
   ngOnInit() {
