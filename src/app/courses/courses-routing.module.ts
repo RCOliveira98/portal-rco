1827ed319c613cff3250572da66b70e5483090bf
RCOliveira98 <respondecodigos@gmail.com>
2019-04-16 11:52:52 -0300

    new configuration view of courses

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -4,9 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoDetailsComponent } from './curso-details/curso-details.component';
 
+// utilização de rotas filhas. Add o array children.
 const routes: Routes = [
-  {path: 'courses', component: CursosComponent},
-  {path: 'course/:id', component: CursoDetailsComponent}
+  {path: 'courses', component: CursosComponent, children: [
+    {path: 'course/:id', component: CursoDetailsComponent},
+    {path: 'course/:id/edit', component: CursoDetailsComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/courses/curso-details/curso-details.component.ts
+++ src/app/courses/curso-details/curso-details.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from './../courses.service';
 import { CursoModel } from '../curso.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-details',
   templateUrl: './curso-details.component.html',
   styleUrls: ['./curso-details.component.css']
 })
-export class CursoDetailsComponent implements OnInit {
+export class CursoDetailsComponent implements OnInit, OnDestroy {
 
   public currentCourse: CursoModel;
+  public myInscricao: Subscription;
 
   constructor(private itemRouter: ActivatedRoute,
               private serviceCourseDetails: CoursesService) { }
@@ -20,13 +22,33 @@ export class CursoDetailsComponent implements OnInit {
     this.init();
   }
 
+  ngOnDestroy() {
+    this.myInscricao.unsubscribe();
+  }
+
+  /*
   private init(): void {
     const date = 'id';
     const courseId = this.itemRouter.snapshot.params[date];
     /* The first element of an array is always positioned in the zero index
      * O primeiro elemento de um array sempre está posicionado no índice zero
-     */
+     *
     this.currentCourse = this.serviceCourseDetails.getCursosById(courseId - 1);
   }
+  */
+ // atendo as mudanças dos parametros das rotas.
+  private init(): void {
+    const date = 'id';
+    let courseId = -1;
+    this.myInscricao = this.itemRouter.params.subscribe(
+      // passando uma function como argumento (callback)
+      (myParams: any) => {
+        courseId = myParams[date];
+        this.currentCourse = this.serviceCourseDetails.getCursosById(courseId - 1);
+      }
+    );
+  }
+
+
 
 }
