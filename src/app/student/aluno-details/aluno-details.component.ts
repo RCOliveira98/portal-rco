20ccc29f905771db4b2cdb7d4a1981b92ec53080
RCOliveira98 <respondecodigos@gmail.com>
2019-04-16 15:01:30 -0300

    COnfigurando o component aluno-details

--- src/app/student/aluno-details/aluno-details.component.ts
+++ src/app/student/aluno-details/aluno-details.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+import { StudentService } from '../student.service';
+import { StudentModel } from './../student.model';
 
 @Component({
   selector: 'app-aluno-details',
   templateUrl: './aluno-details.component.html',
   styleUrls: ['./aluno-details.component.css']
 })
-export class AlunoDetailsComponent implements OnInit {
+export class AlunoDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public currentElement: StudentModel;
+  private inscricao: Subscription;
+
+  constructor(private servStudent: StudentService,
+              private elementRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.init();
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  private init(): void {
+    const date = 'id';
+    let currentId: number;
+    this.inscricao = this.elementRoute.params.subscribe(
+      (myParams: any) => {
+        currentId = myParams[date];
+        this.currentElement = this.servStudent.getStudentById(currentId - 1);
+      }
+    );
   }
 
 }
